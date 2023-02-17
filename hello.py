0bfb313fae68308b3e8c0598d5d26d7c8129984e
Naveen Rana <naveenrana@gmail.com>
2023-02-17 12:21:10 +0000

    added some new functions

--- hello.py
+++ hello.py
@@ -3,13 +3,4 @@ def add(x, y):
 
 result = add(7, 3)
 print(f"this is the sum: 7, 3, {result}")
-
-def toyou(x):
-    return f"hi {x}"
-
-def add(x):
-    return x + 1
-
-def subtract(x):
-    return x - 1
     
